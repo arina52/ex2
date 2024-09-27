bb506136b73360ed9e180b97839053637c8fac89
Arina <mitreanuarina@gmail.com>
2024-09-28 00:43:17 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        long currentWorldPopulation = 7811080815L;
+        int carsProducedThisYear = 55235358;
+        char firstLetter = 'A';
+        boolean javaIsAmazing = true;
+        double pi = 3.14159;
+
+        System.out.println(currentWorldPopulation);
+        System.out.println(carsProducedThisYear );
+        System.out.println(firstLetter);
+        System.out.println(javaIsAmazing);
+        System.out.println(pi);
+
+    }
+}
